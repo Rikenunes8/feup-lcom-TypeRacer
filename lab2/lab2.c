8eb3c6a26de2d2a0e99745ea142e0d5c469288f8
up201704805 <up201704805@fe.up.pt>
2020-10-10 17:19:33 +0100

    util_get_MSB e util_get_LSB implementados mas não testados

--- lab2/lab2.c
+++ lab2/lab2.c
@@ -29,8 +29,8 @@ int main(int argc, char *argv[]) {
   return 0;
 }
 
-int(timer_test_read_config)(uint8_t timer, enum timer_status_field field) {
-  /* To be implemented by the students */
+int(timer_test_read_config)(uint8_t timer, enum timer_status_field field) 
+{
   uint8_t st;
   timer_get_conf(timer, &st);
   timer_display_conf(timer, st, field);
@@ -38,9 +38,12 @@ int(timer_test_read_config)(uint8_t timer, enum timer_status_field field) {
   return 1;
 }
 
-int(timer_test_time_base)(uint8_t timer, uint32_t freq) {
+int(timer_test_time_base)(uint8_t timer, uint32_t freq) 
+{
   /* To be implemented by the students */
   printf("%s is not yet implemented!\n", __func__);
+  timer_set_frequency(timer, freq);
+
 
   return 1;
 }
--- lab2/utils.c
+++ lab2/utils.c
@@ -5,18 +5,19 @@
 int(util_get_LSB)(uint16_t val, uint8_t *lsb) {
   /* To be implemented by the students */
   printf("%s is not yet implemented!\n", __func__);
-
+  lsb = val & 0x0000FFFF 
   return 1;
 }
 
 int(util_get_MSB)(uint16_t val, uint8_t *msb) {
   /* To be implemented by the students */
   printf("%s is not yet implemented!\n", __func__);
-
+  msb = val & 0xFFFF0000
   return 1;
 }
 
-int (util_sys_inb)(int port, uint8_t *value) {  
+int (util_sys_inb)(int port, uint8_t *value) 
+{  
   uint32_t a32;
   
   if (!sys_inb(port, &a32)){
