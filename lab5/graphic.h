6c9191ec38972fd59c0ae81dfa6a2ea7ddceee5b
Henrique Nunes <up201906852@fe.up.pt>
2020-11-27 10:14:44 +0000

    video_test_move falta apagar os frames anteriores

--- lab5/graphic.h
+++ lab5/graphic.h
@@ -5,7 +5,9 @@
 #include <machine/int86.h>
 #include <vbe.h>
 
-int graphic_init(uint16_t mode, vbe_mode_info_t *info, uint8_t vbe_function);
+int graphic_def(vbe_mode_info_t *info);
+
+int graphic_init(uint16_t mode, uint8_t vbe_function);
 
 int graphic_pixel(uint32_t x, uint32_t y, uint32_t color);
 
