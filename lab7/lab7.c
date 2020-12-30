25c78c2988afd383723804a5b03c6886a86237f8
Henrique Ribeiro Nunes <up201906852@fe.up.pt>
2020-12-30 14:19:48 +0000

    Introdução ao lab7

---
+++ lab7/lab7.c
@@ -0,0 +1,43 @@
+// IMPORTANT: you must include the following line in all your C files
+#include <lcom/lcf.h>
+
+#include <stdint.h>
+#include <stdio.h>
+
+// Any header files included below this line should have been created by you
+
+int main(int argc, char *argv[]) {
+  // sets the language of LCF messages (can be either EN-US or PT-PT)
+  lcf_set_language("EN-US");
+
+  // enables to log function invocations that are being "wrapped" by LCF
+  // [comment this out if you don't want/need it]
+  lcf_trace_calls("/home/lcom/labs/lab7/trace.txt");
+
+  // enables to save the output of printf function calls on a file
+  // [comment this out if you don't want/need it]
+  lcf_log_output("/home/lcom/labs/lab7/output.txt");
+
+  // handles control over to LCF
+  // [LCF handles command line arguments and invokes the right function]
+  if (lcf_start(argc, argv))
+    return 1;
+
+  // LCF clean up tasks
+  // [must be the last statement before return]
+  lcf_cleanup();
+
+  return 0;
+}
+
+int ser_test_conf(unsigned short base_addr) {
+    /* To be completed */
+	return 0;
+}
+
+int ser_test_set(unsigned short base_addr, unsigned long bits, unsigned long stop, 
+	           long parity, unsigned long rate) { 
+    /* To be completed */
+	return 0;
+}
+
